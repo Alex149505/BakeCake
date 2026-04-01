3589872c1511a817b02666caef2f9009215d51e7
doingmov <yohei13111@mail.ru>
2026-04-01 17:30:05 +0300

    Delete orders.py

--- orders.py
+++
@@ -1,9 +0,0 @@
-user_orders = {}
-
-def save_order(chat_id, order_data):
-    if chat_id not in user_orders:
-        user_orders[chat_id] = []
-    user_orders[chat_id].append(order_data)
-
-def get_user_orders(chat_id):
-    return user_orders.get(chat_id, [])
